ace96613c977c1dbd9e07504ec76fed4cae9c7e6
bubbleship <84926115+bubbleship@users.noreply.github.com>
2023-03-07 15:53:30 +0200

    Add remember() to Agent class

--- src/main/agent.py
+++ src/main/agent.py
@@ -1,6 +1,8 @@
 from collections import deque
 
-from consts import Consts
+from numpy import ndarray
+
+from main.consts import Consts
 
 
 class Agent:
@@ -10,3 +12,7 @@ class Agent:
 		self.epsilon = 0
 		self.gamma = 0
 		self.memory = deque(maxlen=Consts.MAX_MEMORY)
+
+	def remember(self, previous_state: ndarray, action: list[3], reward: int, next_state: ndarray,
+				 is_game_over: bool) -> None:
+		self.memory.append((previous_state, action, reward, next_state, is_game_over))
