f2ea9e4e4cc03e88e9a4ef0d1cd72151876afe49
bubbleship <84926115+bubbleship@users.noreply.github.com>
2023-03-08 18:57:07 +0200

    Add forward() to LinearQNet class

--- src/main/model.py
+++ src/main/model.py
@@ -1,4 +1,7 @@
 import torch.nn as nn
+import torch.nn.functional as functional
+from torch import Tensor
+
 
 class LinearQNet(nn.Module):
 
@@ -6,3 +9,8 @@ class LinearQNet(nn.Module):
 		super().__init__()
 		self.input_to_hidden = nn.Linear(input_layer_size, hidden_layer_size)
 		self.hidden_to_output = nn.Linear(hidden_layer_size, output_layer_size)
+
+	def forward(self, x: Tensor) -> Tensor:
+		x = functional.relu(self.input_to_hidden(x))
+		x = self.hidden_to_output(x)
+		return x
