4cc1597dd8e64b111e07c3ab14999fe2ec11c502
bubbleship <84926115+bubbleship@users.noreply.github.com>
2023-03-08 20:30:10 +0200

    Add model and trainer properties to Agent class

--- src/main/agent.py
+++ src/main/agent.py
@@ -3,6 +3,7 @@ from collections import deque
 from numpy import ndarray
 
 from main.consts import Consts
+from main.model import LinearQNet, QTrainer
 
 
 class Agent:
@@ -10,8 +11,11 @@ class Agent:
 	def __init__(self):
 		self.games_count = 0
 		self.epsilon = 0
-		self.gamma = 0
+		self.gamma = 0.9  # Discount rate
 		self.memory = deque(maxlen=Consts.MAX_MEMORY)
+		self.model = LinearQNet(Consts.MODEL_INPUT_LAYER_SIZE, Consts.MODEL_HIDDEN_LAYER_SIZE,
+								Consts.MODEL_OUTPUT_LAYER_SIZE)
+		self.trainer = QTrainer(self.model, Consts.LEARNING_RATE, self.gamma)
 
 	def remember(self, previous_state: ndarray, action: list[3], reward: int, next_state: ndarray,
 				 is_game_over: bool) -> None:
