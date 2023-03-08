db5ec5e8378dd0492545674c07478f372debcb69
bubbleship <84926115+bubbleship@users.noreply.github.com>
2023-03-08 20:04:11 +0200

    Add train_step() to QTrainer class

--- src/main/model.py
+++ src/main/model.py
@@ -3,6 +3,7 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as functional
+from numpy import ndarray
 from torch import Tensor, optim
 
 
@@ -34,3 +35,33 @@ class QTrainer:
 		self.gamma = gamma
 		self.optimizer = optim.Adam(model.parameters(), lr)
 		self.criterion = nn.MSELoss()
+
+	def train_step(self, previous_state: ndarray, action: list[3], reward: int, next_state: ndarray,
+				   is_game_over: bool) -> None:
+		previous_state = torch.tensor(previous_state, dtype=torch.float)
+		next_state = torch.tensor(next_state, dtype=torch.float)
+		action = torch.tensor(action, dtype=torch.long)
+		reward = torch.tensor(reward, dtype=torch.float)
+
+		if len(previous_state.shape) == 1:
+			previous_state = torch.unsqueeze(previous_state, 0)
+			next_state = torch.unsqueeze(next_state, 0)
+			action = torch.unsqueeze(action, 0)
+			reward = torch.unsqueeze(reward, 0)
+			is_game_over = (is_game_over, )
+
+		prediction = self.model(previous_state)
+
+		target = prediction.clone()
+		for i in range(len(is_game_over)):
+			new_q = reward[i]
+			if not is_game_over[i]:
+				new_q = reward[i] + self.gamma * torch.max(self.model(next_state[i]))
+
+			target[i][torch.argmax(action[i]).item()] = new_q
+
+		self.optimizer.zero_grad()
+		loss = self.criterion(target, prediction)
+		loss.backward()
+
+		self.optimizer.step()
