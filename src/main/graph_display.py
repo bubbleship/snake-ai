5f49456695aa3fac83ac18ab1a401cbb80dbe7de
bubbleship <84926115+bubbleship@users.noreply.github.com>
2023-06-11 14:32:53 +0300

    Update graph_display.py  Improve graph appearance

--- src/main/graph_display.py
+++ src/main/graph_display.py
@@ -26,16 +26,21 @@ class History:
 
 	@staticmethod
 	def _plot_history():
-		pyplot.gcf()
-		pyplot.clf()
-		pyplot.title('Training history')
-		pyplot.xlabel('Number of Games')
-		pyplot.ylabel('Score')
-		pyplot.plot(History.scores_history)
-		pyplot.plot(History.scores_average_history)
-		pyplot.ylim(ymin=0)
-		pyplot.text(len(History.scores_history) - 1, History.scores_history[-1], str(History.scores_history[-1]))
-		pyplot.text(len(History.scores_average_history) - 1, History.scores_average_history[-1],
-					str(History.scores_average_history[-1]))
+		figure, axes = pyplot.subplots(nrows=1, ncols=1)
+
+		figure.canvas.manager.set_window_title("Training History")
+		axes.set_title("Training History")
+		axes.set_xlabel("Game #")
+		axes.set_ylabel("Score")
+		axes.set_xticks(range(History.episodes_count), range(1, History.episodes_count + 1))
+		axes.plot(History.scores_history, color="green", marker="o")
+		axes.plot(History.scores_average_history, color="blue", marker="o")
+		axes.legend(["Score", "Average"])
+		axes.grid(True)
+		axes.set_ylim(ymin=0)
+		axes.set_xlim(xmin=0)
+		if History.episodes_count != 0:
+			axes.text(len(History.scores_history) - 1, History.scores_history[-1], str(History.scores_history[-1]))
+			axes.text(len(History.scores_average_history) - 1, History.scores_average_history[-1],
+					  str(History.scores_average_history[-1]))
 		pyplot.show(block=True)
-		pyplot.pause(.1)
